df6d3d772cf6024ff70b4ab97ef17f7cc7812d4c
Luiza <luizarafaelapadilha@gmail.com>
2025-01-18 15:11:41 -0300

    init project

---
+++ app/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center fill-height mx-auto" max-width="900">
+      <p>content here</p>
+    </v-responsive>
+  </v-container>
+</template>
