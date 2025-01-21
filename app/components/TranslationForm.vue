934f04a515ddb8509ea1d7cc0445a59a7f1fdb1f
Luiza <luizarafaelapadilha@gmail.com>
2025-01-21 20:15:34 -0300

    finish form style

--- app/components/TranslationForm.vue
+++ app/components/TranslationForm.vue
@@ -1,63 +1,73 @@
 <template>
   <v-form @submit.prevent>
-    <v-text-field v-model="userInput" label="Text" variant="underlined" />
     <v-row>
-      <v-col cols="12" md="6">
+      <v-col>
+        <v-text-field v-model="userInput" label="Texto para tradução:" />
+      </v-col>
+    </v-row>
+
+    <v-row class="mt-4 mt-md-8">
+      <v-col cols="12" md="5">
         <v-select
-          label="From"
-          :items="[
-            'California',
-            'Colorado',
-            'Florida',
-            'Georgia',
-            'Texas',
-            'Wyoming',
-          ]"
-          variant="underlined"
+          v-model="language_from"
+          label="De:"
+          :items="languages_list"
+          item-title="language"
+          item-value="language"
         />
       </v-col>
-      <v-col cols="12" md="6">
+      <v-col
+        cols="12"
+        md="2"
+        class="d-flex flex-row justify-center py-0 py-md-2"
+      >
+        <v-btn
+          class="ma-2 d-none d-md-flex"
+          variant="text"
+          icon="mdi-swap-horizontal"
+        />
+        <v-btn class="ma-2 d-md-none" variant="text" icon="mdi-swap-vertical" />
+      </v-col>
+      <v-col cols="12" md="5">
         <v-select
-          label="To"
-          :items="[
-            'California',
-            'Colorado',
-            'Florida',
-            'Georgia',
-            'Texas',
-            'Wyoming',
-          ]"
-          variant="underlined"
+          v-model="language_to"
+          label="Para:"
+          :items="languages_list"
+          item-title="language"
+          item-value="language"
         />
       </v-col>
     </v-row>
-    <v-btn
-      class="btn-translate mt-2"
-      block
-      variant="tonal"
-      :disabled="loading"
-      @click="fetchChatCompletion"
-      >{{ loading ? "Carregando..." : "Enviar" }}</v-btn
-    >
-    <v-btn
-      v-if="completionMessage"
-      class="mt-2"
-      block
-      variant="tonal"
-      @click="completionMessage = ''"
-      >Reset</v-btn
-    >
-    {{ completionMessage }}
+
+    <v-row class="mt-4 mt-md-8">
+      <v-col cols="12">
+        <v-btn
+          class="btn-translate"
+          block
+          variant="tonal"
+          :disabled="loading"
+          @click="fetchChatCompletion"
+          >{{ loading ? "Carregando..." : "Traduzir" }}</v-btn
+        >
+      </v-col>
+      <v-col>
+        <v-btn block variant="tonal" to="/">Voltar</v-btn>
+      </v-col>
+    </v-row>
   </v-form>
 </template>
   
   <script setup>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 import { getGroqChatCompletion } from "@/services/groqService";
+import languages from "../assets/languages.json";
 
 const userInput = ref("");
 const completionMessage = ref("");
 const loading = ref(false);
+const languages_list = reactive(languages);
+const language_from = ref("Português");
+const language_to = ref("Inglês");
 
 async function fetchChatCompletion() {
   if (!userInput.value) {
